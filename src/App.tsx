c67c0924a4b6d760d1eae7aeddd24002ab18d07e
Katie Klein <katieklein07@gmail.com>
2024-07-04 21:34:01 -0500

    Move data load to home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,14 @@ import Home from "./pages/Home";
 import Stats from "./pages/Stats";
 import Tracker from "./pages/Tracker";
 import Details from "./pages/Details";
-import { doc, getDoc } from "firebase/firestore";
-import { collection, query, where, getDocs } from 'firebase/firestore';
-import { db } from './firebase';
-
-
-// Hardcoding specific PC id for now since it will mainly be used for a single character.
-// TODO - implement a search function before loading data so that different PCs can be displayed
-const PC_ID="kvneaiw2pulEBICGf6Dm";
 
 function App() {
-    const loadData = () => {
-        console.log('Loading data');
-        const getPCData = async () => {
-            try {
-                console.log('Getting base character details.');
-                // Get base character details
-                const docRef = doc(db, "pcBaseDetails", PC_ID);
-                const docSnap = await getDoc(docRef);
-
-                if (docSnap.exists()) {
-                    console.log("Document data:", docSnap.data());
-                } else {
-                    console.log("No such document!");
-                    return;
-                }
-
-                // Get ability scores
-                console.log('getting ability scores.');
-                const asQuery = query(collection(db, "abilityScores"), where("pcId", "==", PC_ID));
-                const asQuerySnapshot = await getDocs(asQuery);
-                asQuerySnapshot.forEach((doc) => {
-                    console.log(doc.id, " => ", doc.data());
-                });
-            } catch (e) {
-                throw Error(JSON.stringify(e));
-            }
-        };
-        return getPCData();
-    };
-    const data = loadData();
-
-    // Load pages
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route index element={<Home/>}/>
-                    <Route path="/home" element={<Home data={data}/>}/>
+                    <Route path="/home" element={<Home/>}/>
                     <Route path="/stats" element={<Stats/>}/>
                     <Route path="/tracker" element={<Tracker/>}/>
                     <Route path="/details" element={<Details/>}/>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,47 @@ import Navbar from "../components/Navbar";
 import { OsoniaSilverhand as pc } from "../data/playerCharacters/OsoniaSilverhand";
 import ImageCard from "../components/cards/ImageCard";
 import Footer from "../components/Footer";
+import { doc, getDoc } from "firebase/firestore";
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
+import { formatDataAsTable } from "../components/utils";
+const config = require('../data/config.json');
 
-function Home(data: any) {
+function Home() {
+    const loadData = () => {
+        console.log('Loading data');
+        const getPCData = async () => {
+            try {
+                console.log('Getting base character details.');
+                // Get base character details
+                const docRef = doc(db, "pcBaseDetails", config.pcId);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                } else {
+                    console.log("No such document!");
+                    return {};
+                }
+
+                // Get ability scores
+                console.log('getting ability scores.');
+                const asQuery = query(collection(db, "abilityScores"), where("pcId", "==", PC_ID));
+                const asQuerySnapshot = await getDocs(asQuery);
+                asQuerySnapshot.forEach((doc) => {
+                    console.log(doc.id, " => ", doc.data());
+                });
+
+                // Format
+                return {...docSnap.data, ...asQuerySnapshot}
+            } catch (e) {
+                throw Error(JSON.stringify(e));
+            }
+        };
+        return getPCData();
+    };
+    const data = loadData();
+
     const pcImagePath = `../src/assets/images/${pc.name.firstName.toLowerCase()}_${pc.name.lastName.toLowerCase()}.png`;
     const pcFullName = `${pc.name.firstName} ${pc.name.lastName}`
     const pcDescription = 'The blacksmith fighter who just wants to help people by getting as strong as possible.';
@@ -21,7 +60,9 @@ function Home(data: any) {
     return (
         <>
             <Navbar/>
-            <p>{JSON.stringify(data)}</p>
+            <div>
+                {formatDataAsTable(data)}
+            </div>
             <ImageCard title={pcFullName} description={pcDescription} imagePath={pcImagePath} data={listCardObject}/>
             <Footer/>
         </>
