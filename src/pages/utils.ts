95d637e8980451f3effb4dc906aa28ef94f27dc7
Katie Klein <katieklein07@gmail.com>
2025-10-12 00:25:24 -0500

    Fix type errors

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -97,6 +97,7 @@ export const handleSubmitEdit = async (
             sourceUrl: formData.sourceUrl,
             displayAsPool: formData.displayAsPool,
             tags: formData.tags,
+            arrayIndex: formData.arrayIndex, // TODO: update as needed
         }
     }
     await updateById(CollectionName.FEATURES, formData.featureId, updatedFeature.data);
